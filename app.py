c659b16e37baeee9c812938e21a8f5218e08332f
connor <caschm@gmail.com>
2023-12-13 11:28:27 -0500

    add basic styling

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import surveys
 # Flask setup 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 # Define current survey we're working with
